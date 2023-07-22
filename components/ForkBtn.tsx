76b4c0ab8115083490e0d81457ac0f2b3d5abb1a
annilq <410493517@qq.com>
2023-07-22 16:01:55 +0800

    update  

--- components/ForkBtn.tsx
+++ components/ForkBtn.tsx
@@ -7,9 +7,9 @@ const ForkButton = (props: { data: CollectionProps }) => {
     const { data } = props
     return (
         <Link href={"/"}>
-            <div className='w-16 flex justify-center items-center gap-1 bg-[#3B29AE] rounded-sm'>
+            <div className='w-16 flex justify-center items-center gap-1 bg-indigo-800 rounded-sm'>
                 <Image alt='fork' src={opensealogo} width={16} height={16} />
-                <div>Buy</div>
+                <div>Fork</div>
             </div>
         </Link>
     )
