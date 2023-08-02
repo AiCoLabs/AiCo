024fb5e3b4b20ff913848c5526548991072629c3
zhujieping <jiezidexiaowo@163.com>
2023-08-02 23:40:53 +0800

    fix: mt change

--- app/NFT/Fork/[id]/[nftId]/[paramId]/page.tsx
+++ app/NFT/Fork/[id]/[nftId]/[paramId]/page.tsx
@@ -6,7 +6,7 @@ export default function ForkNFT({ params }: { params: { id: string, nftId: strin
   return (
     <div className="container mx-auto var-dark">
       <Separator className="my-6" />
-      <div className="flex-1 text-white text-2xl mt-32">
+      <div className="flex-1 text-white text-2xl mt-8">
         <NftBaseForm type="ForkImage" className="w-2/3" collectionId={id} nftId={nftId} fromImageId={paramId}/>
       </div>
     </div>
